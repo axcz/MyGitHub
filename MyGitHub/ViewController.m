e97fc3a8bdadf8d846cca706fdfc2b4870bcb0b1
scsys <scsys@anjinliang.local>
2016-03-04 11:24:14 +0800

    GitHub初次使用

---
+++ MyGitHub/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MyGitHub
+//
+//  Created by scsys on 16/3/4.
+//  Copyright © 2016年 scsys. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   //1.0版本已完成
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
